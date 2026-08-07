28b3462537a53613b8772ae6e02b786cc1b6948f
RicardoGuido <rcrdguido@gmail.com>
2026-08-06 22:22:28 -0300

    criação js e carrossel 06/08

---
+++ js/interacoes.js
@@ -0,0 +1,24 @@
+// Menu hamburguer
+
+function clickMenu() {
+    let itens = document.getElementById("itens");
+
+    if (itens.style.display === "block" || itens.style.display === "") {
+        itens.style.display = "none"
+    } else {
+        itens.style.display = "block";
+    }
+
+}
+
+
+// Carrossel
+
+$(document).ready(function () {
+    $('#destaque').slick({
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false
+    });
+});
